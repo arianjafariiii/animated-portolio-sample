562348146a6467d55e3b33fc60925611cf506aa1
ArianJafari <arian.jafari2233@gmail.com>
2025-08-31 18:42:55 +0330

    Super hero section completed.

---
+++ script.js
@@ -0,0 +1,37 @@
+const circleImg = document.querySelector(".circle img");
+const nameContainer = document.querySelector(".name-container");
+const hiddenBackImg = document.querySelector(".hidden-back img");
+const experience = document.querySelector(".experience");
+const quote = document.querySelector(".quote")
+circleImg.addEventListener("mouseenter", () => {
+    nameContainer.style.animation = "textOut 0.5s linear forwards";
+    experience.style.animation= "textUp 0.5s linear forwards";
+    hiddenBackImg.style.animation=" backIn 0.5s linear forwards"
+    setTimeout(() => {
+        experience.style.top= "20%";
+        quote.style.top= "20%";
+        hiddenBackImg.style.display= "flex";
+    }, 500);
+    quote.style.animation= "textUp 0.5s linear forwards"
+    
+    
+    
+});
+circleImg.addEventListener("mouseleave", () => {
+    nameContainer.style.animation = "textIn 0.5s linear ";   // restore
+    experience.style.animation= "textDown 0.5s linear forwards";
+    hiddenBackImg.style.animation="backOut 0.5s linear forwards";
+    setTimeout(() => {
+        experience.style.top= "40%";
+        quote.style.top= "40%";
+        hiddenBackImg.style.display= "none";
+        
+    }, 500);
+    quote.style.animation= "textDown 0.5s linear forwards"
+    
+});
+
+
+
+
+
